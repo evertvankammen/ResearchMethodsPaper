6433501109e822e827f8d3cf353fa5b11aa66315
evert <evert.vankammen@qfactors.nl>
2024-08-05 22:26:19 +0200

    Refactor sections and enhance methodology description
    Renamed "Results" section to "Artefact" and expanded its content with detailed agent behaviors and system operations. Improved "Research methodology" section with clearer steps for problem formulation, conceptual validation, computer model translation, and simulation execution, adding references to related models and ensuring consistent narrative flow.

---
+++ src/graphs/fooddelivery.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+import numpy as np
+
+def get_chance_minute(x):
+    if (7 * 60) <= x < (8 * 60):
+        return 2 / (24 * 60)
+
+    if (12 * 60) <= x < (13 * 60):
+        return 2 / (24 * 60)
+
+    if (18 * 60) <= x < (20 * 60):
+        return 7 / (24 * 120)
+
+    return 13 / (24 * 20 * 60)
+
+def get_data_frame():
+    minutes = range(0, 24 * 60)
+    time = []
+    p = []
+    for t in minutes:
+        p.append(get_chance_minute(t)/7)
+        time.append(t)
+
+    print(sum(p))
+
+    data = {'time': time, 'p': p}
+    df = pd.DataFrame(data)
+
+    return df
+
+
+def print_food_ordering():
+    df = get_data_frame()
+    sns.set_style("darkgrid")
+    sns.set_context("paper", rc={"grid.linewidth": 0.6})
+    plot = sns.lineplot(data=df, x="time", y="p")
+    plot.set(xlabel="time of day", ylabel="probability of ordering food per minute")
+    plt.yticks(np.arange(0, 0.0005, step=0.00005))
+    plt.xticks(np.arange(0, 24 * 60 + 1, step=120))
+
+    x_labels = []
+
+    for x in plot.get_xticks():
+        hour = int(x / 60)
+        x_labels.append(f"{hour}")
+
+    plot.set_xticklabels(x_labels)
+    plt.show()
+    fig = plot.get_figure()
+    fig.savefig("food_ordering_distribution.png")
+
+print_food_ordering()
